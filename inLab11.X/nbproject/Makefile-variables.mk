d522e67226aff0681bfba3e9bf411ab2aa4b89f1
Westly Bouchard <westly.bouchard@gmail.com>
2025-11-14 10:55:16 -0700

    In Lab 11

---
+++ inLab11.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=inLab11.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/inLab11.X.production.hex
